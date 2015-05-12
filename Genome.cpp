a12773e6b38cfbd2ac5d1d4b7ef55f6507e23d89
Jean Bertrand <jeanus24@gmail.com>
2015-05-12 11:31:09 +0200

    Array in 1 dimension

--- Genome.cpp
+++ Genome.cpp
@@ -68,11 +68,11 @@ int ** Genome::toArrays()
 	int i;
 	int ** res = 0;
 	res = new int*[this->size];
-	for (i = 0; i < this->size; i++)
+	for (i = 0; i < this->size*2; i++)
 	{
-		res[i] = new int[2];
-		res[i][0] = this->adn.at(i).direct;
-		res[i][1] = this->adn.at(i).steps;
+		res[i] = this->adn.at(i).direct;
+		i++;
+		res[i] = this->adn.at(i).steps;
 	}
 	return res;
 }
