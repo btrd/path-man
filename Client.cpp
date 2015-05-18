49654c2d3bb23c0f5b84c58e63ab1ae17914ad50
Jiloko <gwenael.larmet@gmail.com>
2015-05-18 17:16:44 +0200

    Draw path of a Random Genome

--- Client.cpp
+++ Client.cpp
@@ -1,6 +1,7 @@
 #include "Village.h"
 #include "callRpc.h"
 #include "population_struct.h"
+#include "Genome.h"
 
 #include <SFML/Graphics.hpp>
 
@@ -63,61 +64,116 @@ void drawmap(Village *v)
 	Map *map = v->getMap();
 	int m = map->getm();
     int n = map->getn();
+    int x,y;
     
     vector<Point*> points = map->getPoints();
     vector<sf::RectangleShape> pixels;
     Point * tmp_point;
-
+    Genome * best;
+    Gene gene;
     sf::RenderWindow window(sf::VideoMode(n * TILE_SIZE, m * TILE_SIZE), "Map Rendering Test");
-    
+    	
 
-    for (i = 0; i < n; i++)
+    while (window.isOpen()) //Boucle principale
     {
-        for (j = 0; j < m; j++)
-        {
-            sf::RectangleShape tile(sf::Vector2f(TILE_SIZE, TILE_SIZE));
-            tile.setSize(sf::Vector2f(TILE_SIZE, TILE_SIZE));
-            tile.setOutlineThickness(1);
-            tile.setOutlineColor(sf::Color(0, 0, 0));
-            
-            tmp_point = points.at(i*m+j);
-
-            switch(tmp_point->getValue())
-            {
-            	case 0: //normal
-            		tile.setFillColor(sf::Color::Green);
-            		break;
-            	case 1: //Wall
-            		tile.setFillColor(sf::Color::White);
-            		break;
-            	case 2: //Start
-            		tile.setFillColor(sf::Color::Red);
-            		break;
-            	case 3: //End
-            		tile.setFillColor(sf::Color::Blue);
-            		break;
-            	default: //default (mmap invalid)
-            		cout << "'" << tmp_point->getValue()<< "'" << endl;
-            		tile.setFillColor(sf::Color::Magenta);
-            		break;
-            }
-            tile.setPosition(tmp_point->getX()*TILE_SIZE, tmp_point->getY()*TILE_SIZE);
-            
-            pixels.push_back(tile);
-        } 
-    }
-
-    while (window.isOpen())
-    {
+    	//Close Event
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        //---------Redraw Window
+        //Prepare Map 
+	    for (i = 0; i < n; i++)
+	    {
+	        for (j = 0; j < m; j++)
+	        {
+	            sf::RectangleShape tile(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+	            tile.setSize(sf::Vector2f(TILE_SIZE, TILE_SIZE));
+	            tile.setOutlineThickness(1);
+	            tile.setOutlineColor(sf::Color(0, 0, 0));
+	            
+	            tmp_point = points.at(i*m+j);
+
+	            switch(tmp_point->getValue())
+	            {
+	            	case 0: //normal
+	            		tile.setFillColor(sf::Color::Green);
+	            		break;
+	            	case 1: //Wall
+	            		tile.setFillColor(sf::Color::White);
+	            		break;
+	            	case 2: //Start
+	            		tile.setFillColor(sf::Color::Red);
+	            		break;
+	            	case 3: //End
+	            		tile.setFillColor(sf::Color::Blue);
+	            		break;
+	            	default: //default (mmap invalid)é
+	            		tile.setFillColor(sf::Color::White);
+	            		break;
+	            }
+	            tile.setPosition(tmp_point->getX()*TILE_SIZE, tmp_point->getY()*TILE_SIZE);
+	            
+	            pixels.push_back(tile);
+	        } 
+	    }
 
         window.clear();
-        for (i = 0; i < n; i++)
+        
+        best = v->getPersonTest()->getGenome();
+        tmp_point = map->getStart();
+        x = tmp_point->getX();
+        y = tmp_point->getY();
+
+        for (i = 0; i < best->getSize()-1; i++)
+        {
+        	gene = best->getAdn().at(i);
+			for (j = 0; j < gene.steps; j++)
+			{
+				switch(gene.direct)
+				{
+					case NORTH:
+						--y;
+						break;
+					case NORTH_EAST:
+						--y;
+						++x;
+						break;
+					case EAST:
+						++x;
+						break;
+					case SOUTH_EAST:
+						++y;
+						++x;
+						break;
+					case SOUTH:
+						++y;
+						break;
+					case SOUTH_WEST:
+						++y;
+						--x;
+						break;
+					case WEST:
+						--x;
+						break;
+					case NORTH_WEST:
+						--y;
+						--x;
+						break;
+				}
+				if (x <= 0 || y <= 0 || x >= m || y >=n)
+				{
+					i = best->getSize();
+					break;
+				}
+				pixels.at(y*n+x).setFillColor(sf::Color::Magenta);
+			}
+        }
+
+
+        for (i = 0; i < n; i++) //draw map
         {
             for (j = 0; j < m; j++)
             {
@@ -125,11 +181,18 @@ void drawmap(Village *v)
             }
         }
         window.display();
+
+        //----------End Redraw
     }
 }
 
 int main(int argc, char const *argv[])
 {
+	if(argc != 2)
+	{
+		cout << "Usage : ./Client <idvillage>" << endl;
+		return -1;
+	}
 	Village *v = new Village(atoi(argv[1]), 10);
 	drawmap(v);
 	struct population_struct pop = v->getPopulationStruct();
